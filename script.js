f1e7817f46bf9312e2a68b393cabaa5af83b6e87
Marsleyy <44040638+Marsleyy@users.noreply.github.com>
2019-02-13 17:11:12 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function(){ 
+    var input1= $("#catordog").val();
+    var input2= $("#outside").val()*10;
+    var input3= $("#age").val();
+    
+$("#total").text(input2);
+});
